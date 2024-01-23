9220a0e3982a5563387d89a3e832d1026a2b10d8
KishorAsabe <kishorasabe2020@gmail.com>
2024-01-23 09:01:09 +0530

    feature pages coming soon

---
+++ src/pages/Document.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Document = () => {
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <div className="bg-white p-8 rounded-md shadow-md">
+        <h1 className="text-2xl font-bold text-red-500 mb-4">
+           Feature is coming soon ......
+        </h1>
+      </div>
+    </div>
+  );
+};
+
+export default Document;
