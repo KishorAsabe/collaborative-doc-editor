eb4222cf64ac3e8cd09cdee4279375a96b3939b2
KishorAsabe <kishorasabe2020@gmail.com>
2024-01-23 08:35:49 +0530

    routes added

--- src/App.js
+++ src/App.js
@@ -1,18 +1,28 @@
-import TextEditor from "./TextEditor"
-import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
+import React from 'react';
+import { Route, Routes, Navigate } from 'react-router-dom';
 
-import { v4 as uuidV4 } from "uuid"
+import TextEditor from './components/TextEditor'; // Import the TextEditor component
+import { v4 as uuidV4 } from 'uuid';
+import Login from './components/Login';
+import Signup from './components/Signup';
+import PrivateRoute from './components/PrivateRoute';
+import Error from './pages/Error';
 
-function App() {
+const App = () => {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Navigate to={`/documents/${uuidV4()}`} />} />
-        <Route path="/documents/:id" element={<TextEditor />} />
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path="/" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
+
+      {/* Use PrivateRoute for the private document route */}
+      
+        <Route path="/documents/:id" element={<PrivateRoute><TextEditor /></PrivateRoute>} />
+      
+      {/* The route below is used to redirect to a new document with a generated ID */}
+      <Route path="/document" element={<Navigate to={`/documents/${uuidV4()}`} />} />
+      <Route path="*" element={<Error />} />
+    </Routes>
   );
-}
+};
 
-
-export default App
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
-import React from "react"
-import ReactDOM from "react-dom"
-import App from "./App"
-import "./index.css"
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
+import "tw-elements-react/dist/css/tw-elements-react.min.css";
+import "./index.css";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+      <Toaster />
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
-)
+);
