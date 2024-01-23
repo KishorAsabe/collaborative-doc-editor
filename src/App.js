0b31e77fe7f41b5bcfcee4c365047cb9a26d8f55
KishorAsabe <kishorasabe2020@gmail.com>
2024-01-23 09:00:39 +0530

    to save docs page

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import { v4 as uuidV4 } from 'uuid';
 import Login from './components/Login';
 import Signup from './components/Signup';
 import PrivateRoute from './components/PrivateRoute';
+import Logout from './components/Logout';
+import Document from './pages/Document';
 import Error from './pages/Error';
 
 const App = () => {
@@ -16,10 +18,12 @@ const App = () => {
 
       {/* Use PrivateRoute for the private document route */}
       
-        <Route path="/documents/:id" element={<PrivateRoute><TextEditor /></PrivateRoute>} />
+      <Route path="/documents/:id" element={<PrivateRoute><TextEditor /></PrivateRoute>} />
       
       {/* The route below is used to redirect to a new document with a generated ID */}
       <Route path="/document" element={<Navigate to={`/documents/${uuidV4()}`} />} />
+      <Route path='/logout' element={<Logout/>}/>
+      <Route path='/saveddocs' element={<Document/>}/>
       <Route path="*" element={<Error />} />
     </Routes>
   );
